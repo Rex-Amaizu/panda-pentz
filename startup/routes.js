86a41cee17513a81b6ab76f45e384c3face6a071
Rex Amaizu <intanalegacy@yahoo.com>
2022-04-18 15:48:22 +0100

    enabled cors

--- startup/routes.js
+++ startup/routes.js
@@ -1,9 +1,11 @@
 const express = require('express');
+const cors=require('cors');
 const nfts = require('../routes/nfts');
 const error = require('../middleware/error');
 
 module.exports = function(app) {
     app.use(express.json());
+    app.use(cors());
     app.use('/api/nfts', nfts);
 
     app.use(error);
